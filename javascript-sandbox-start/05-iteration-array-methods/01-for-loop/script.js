b02773070eacc8a267bcfccb2d15c4dffe6e55bb
Total Gobshite <104657642+Total-Gobshite@users.noreply.github.com>
2023-05-27 21:19:41 +0100

    note this bitch up

--- javascript-sandbox-start/05-iteration-array-methods/01-for-loop/script.js
+++ javascript-sandbox-start/05-iteration-array-methods/01-for-loop/script.js
@@ -26,3 +26,8 @@ console.log(`There are ${animals.length} animals in the array.`);
 for (let i = 0; i < animals.length; i++) { // goes through entire array
     console.log(i, animals[i] === 'monkey' ? `${animals[i]} are top-tier` : animals[i]);
 }
+
+// these along of for loops are old school now we have higher order array methods -- forEach, forIn, forOf
+
+// Use a for loop when you know the number of times you want the loop to run.
+// Use a while loop when the number of times you want to loop is unknown -- BUT MAKE SURE IT IS NOT INFINITE!
