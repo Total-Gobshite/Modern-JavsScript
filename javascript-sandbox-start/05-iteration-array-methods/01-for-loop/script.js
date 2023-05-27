d75acef16362a78543986be6505fa39e4f31269c
Total Gobshite <104657642+Total-Gobshite@users.noreply.github.com>
2023-05-27 20:12:50 +0100

    for loops

--- javascript-sandbox-start/05-iteration-array-methods/01-for-loop/script.js
+++ javascript-sandbox-start/05-iteration-array-methods/01-for-loop/script.js
@@ -5,3 +5,24 @@
 // CONDITION EXPRESSION - Condition that the loop will continue to run as long as it is met or until the condition is false
 // INCREMENT EXPRESSION - Expression that will be executed after each iteration of the loop. Usually increments the variable
 // STATEMENT - Code that will be executed each time the loop is run. To execute a `block` of code, use the `{}` syntax
+
+// ------ BASIC LOOP ------
+for (let i = 0; i <= 10; i++) {
+    console.log(i);
+}
+
+// ------ NESTED LOOPS ------
+for (let i = 1; i <= 10; i++) {
+    console.log('Outer loop:', i);
+    for (let j = 1; j <= 10; j++) {
+        console.log('Inner loop:', j);
+    }
+}
+
+// ------ LOOPING THROUGH ARRAYS ------
+const animals = ['lion', 'tiger', 'bear', 'monkey', 'elephant', 'giraffe'];
+console.log(`There are ${animals.length} animals in the array.`);
+
+for (let i = 0; i < animals.length; i++) { // goes through entire array
+    console.log(i, animals[i] === 'monkey' ? `${animals[i]} are top-tier` : animals[i]);
+}
