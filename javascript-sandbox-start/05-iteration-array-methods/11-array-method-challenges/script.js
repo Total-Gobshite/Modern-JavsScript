1036166467f0549c42a602c9350f83ea3aa35765
Total Gobshite <104657642+Total-Gobshite@users.noreply.github.com>
2023-05-28 20:29:22 +0100

    array method challenge

--- javascript-sandbox-start/05-iteration-array-methods/11-array-method-challenges/script.js
+++ javascript-sandbox-start/05-iteration-array-methods/11-array-method-challenges/script.js
@@ -36,6 +36,21 @@ const people = [
     age: 23,
   },
 ];
+console.log(people);
+
+const youngPeople = people
+  .filter(person => person.age <= 25)
+  .map(person => ({ name: `${person.firstName} ${person.lastName}`, phone: person.phone }));
+console.log(youngPeople);
 
 // Challenge 2
 const numbers = [2, -30, 50, 20, -12, -9, 7];
+
+const positiveNumbers = numbers.reduce((acc, num) => num > 0 ? acc += num : acc, 0);
+console.log(positiveNumbers);
+
+// Challenge 3
+const words = ['coder', 'programmer', 'developer'];
+
+const capitalizedWords = words.map(word => word[0].toUpperCase() + word.slice(1));
+console.log(capitalizedWords);
