ef180b924773ab69b744e24f063e9776dd22d9f6
Total Gobshite <104657642+Total-Gobshite@users.noreply.github.com>
2023-06-01 08:55:57 +0100

    cleaner code for creating elements

--- javascript-sandbox-start/06-document-object-model/09-refactor-to-multiple-functions/shopping-list/script.js
+++ javascript-sandbox-start/06-document-object-model/09-refactor-to-multiple-functions/shopping-list/script.js
@@ -1,17 +1,52 @@
-function createNewItem(item) {
-  const li = document.createElement('li');
-  li.appendChild(document.createTextNode(item));
+// ------ Old Code ------
+// function createNewItem(item) {
+//   const li = document.createElement('li');
+//   li.appendChild(document.createTextNode(item));
 
+// const button = document.createElement('button');
+// button.className = 'remove-item btn-link text-red';
+
+// const icon = document.createElement('i');
+// icon.className = 'fa-solid fa-xmark';
+
+// button.appendChild(icon);
+//   li.appendChild(button);
+
+//   document.querySelector('.items').appendChild(li);
+// }
+
+// createNewItem('Cheese');
+
+// ------ Clean Code ------
+function createButton(classes) {
   const button = document.createElement('button');
-  button.className = 'remove-item btn-link text-red';
-
-  const icon = document.createElement('i');
-  icon.className = 'fa-solid fa-xmark';
-
+  button.className = classes;
+  const icon = createIcon('fa-solid fa-xmark');
   button.appendChild(icon);
-  li.appendChild(button);
-
-  document.querySelector('.items').appendChild(li);
+  return button;
 }
 
-createNewItem('Cheese');
+function createIcon(classes) {
+  const icon = document.createElement('i');
+  icon.className = classes;
+  return icon;
+}
+
+function createListItem(item) {
+  const li = document.createElement('li'); // create list element
+  li.appendChild(document.createTextNode(item)); // create text and add to list
+
+  const button = createButton('remove-item btn-link text-red'); // create button and icon
+  li.appendChild(button); // add to list
+
+  document.querySelector('.items').appendChild(li); // add list to ul with .items class
+}
+
+createListItem('Eggs');
+
+// <li>
+//   Orange Juice
+//   <button class="remove-item btn-link text-red">
+//     <i class="fa-solid fa-xmark"></i>
+//   </button>
+// </li>
